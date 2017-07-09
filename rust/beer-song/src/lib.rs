ee4a38baf0f134b46e4526fc935c684bc0d73025
Daniel Spirin <l1ks.sds@gmail.com>
2017-07-09 20:04:30 +0300

    Make 'beer' solution a litlle nicer

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,22 +1,51 @@
 pub fn verse(number: u32) -> String {
-    if number == 0 {
-        format!("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n")
-    } else if number == 2 {
-        format!("{0} bottles of beer on the wall, {0} bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n", number)
-    } else if number == 1 {
-        format!("{current} bottle of beer on the wall, {current} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n", current=number)
-    } else {
-        format!("{current} bottles of beer on the wall, {current} bottles of beer.\nTake one down and pass it around, {next} bottles of beer on the wall.\n", current=number, next=number-1)
-    }
+    let remaining_number = 
+        if number == 0 { 
+            99 
+        } else { 
+            number - 1 
+        };
+
+    format!(
+        "{} on the wall, {}.\n{}, {} on the wall.\n",
+        how_many_bottles(number),
+        how_many_bottles(number).to_lowercase(),
+        what_action_to_take(number),
+        how_many_bottles(remaining_number).to_lowercase()
+    )
 }
 
 pub fn sing(start: u32, end: u32) -> String {
     let mut output = verse(start);
-    let mut current = start;
-    while current != end {
-        current -= 1;
+    for i in (end..start).rev() {
         output += &"\n".to_string();
-        output += &verse(current);
+        output += &verse(i);
     }
     output
 }
+
+fn how_many_bottles(number: u32) -> String {
+    match number {
+        0 => "No more bottles of beer".to_string(),
+        1 => "1 bottle of beer".to_string(),
+        _ => format!("{} bottles of beer", number)
+    }
+}
+
+fn what_action_to_take(number: u32) -> &'static str {
+    match number {
+        0 => "Go to the store and buy some more",
+        1 => "Take it down and pass it around",
+        _ => "Take one down and pass it around"
+    }
+}
+
+
+// Another solution from taylorzr
+// pub fn sing(start: u32, end: u32) -> String {
+//     (end..start + 1)
+//         .rev()
+//         .map(verse)
+//         .collect::<Vec<String>>()
+//         .join("\n")
+// }
