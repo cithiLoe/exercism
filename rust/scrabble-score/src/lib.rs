6bec37e241b52d0a919cd5712801a771e502305f
Daniel Spirin <l1ks.sds@gmail.com>
2017-07-11 14:37:56 +0300

    Implement 'scrabble'

---
+++ rust/scrabble-score/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn score(input: &str) -> u32 {
+    input.to_lowercase().chars().map(|c| 
+        match c {
+            'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' => 1,
+            'd' | 'g' => 2,
+            'b' | 'c' | 'm' | 'p' => 3,
+            'f' | 'h' | 'v' | 'w' | 'y'  => 4,
+            'k' => 5,
+            'j' | 'x' => 8,
+            'q' | 'z' => 10,
+            _ => 0
+        }
+    ).sum()
+}
