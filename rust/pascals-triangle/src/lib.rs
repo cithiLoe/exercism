fac56a50301560acf0a04c8e38da2d9a26f562ed
Daniel Spirin <l1ks.sds@gmail.com>
2017-07-11 14:11:08 +0300

    Implement 'pascal triangle'

--- rust/pascals-triangle/src/lib.rs
+++ rust/pascals-triangle/src/lib.rs
@@ -1,11 +1,23 @@
-pub struct PascalsTriangle;
+pub struct PascalsTriangle {
+    row_count: u32,
+}
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!();
+        PascalsTriangle { row_count: row_count }
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        unimplemented!();
+        (0..self.row_count).map(|n| PascalsTriangle::row(n))
+            .collect()
+    }
+
+    fn row(n: u32) -> Vec<u32> {
+        (0..n).fold(vec![1], |mut r, p| {
+            if let Some(&last) = r.last() {
+                r.push(last * (n - p) / (p + 1));
+            } 
+            r
+        })
     }
 }
