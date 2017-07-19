c0ffb2856e3bc525520a28a27c703983dcead191
Daniel Spirin <l1ks.sds@gmail.com>
2017-07-20 00:42:28 +0300

    Implement 'grade school'

--- rust/grade-school/src/lib.rs
+++ rust/grade-school/src/lib.rs
@@ -1,19 +1,25 @@
-#[allow(unused_variables)]
+use std::collections::HashMap;
 
 pub struct School {
+    grades: HashMap<u32, Vec<String>>
 }
 
 impl School {
     pub fn new() -> School {
-        unimplemented!()
+        School { grades: HashMap::new() }
     }
 
     pub fn add(&mut self, grade: u32, student: &str) {
-        unimplemented!()
+        // self.grades.entry(grade).or_insert(Vec::new()).push(student.to_string());
+        let mut entry = self.grades.entry(grade).or_insert(Vec::new());
+        entry.push(student.to_string());
+        entry.sort();
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        unimplemented!()
+        let mut gr = self.grades.keys().cloned().collect::<Vec<u32>>();
+        gr.sort();
+        gr
     }
 
     // If grade returned an `Option<&Vec<String>>`,
@@ -21,6 +27,36 @@ impl School {
     // By returning an owned vector instead,
     // the internal implementation is free to use whatever it chooses.
     pub fn grade(&self, grade: u32) -> Option<Vec<String>> {
-        unimplemented!()
+        self.grades.get(&grade).map(|v| v.iter().cloned().collect())
     }
 }
+// use std::collections::{BTreeMap, BTreeSet};
+
+// pub struct School {
+//     grades: BTreeMap<u32, BTreeSet<String>>
+// }
+
+// impl School {
+//     pub fn new() -> School {
+//         School { grades: BTreeMap::new() }
+//     }
+
+//     pub fn add(&mut self, grade: u32, student: &str) {
+//         self.grades
+//             .entry(grade)
+//             .or_insert(BTreeSet::new())
+//             .insert(student.to_string());
+//     }
+
+//     pub fn grades(&self) -> Vec<u32> {
+//         self.grades.keys().cloned().collect::<Vec<u32>>()
+//     }
+
+//     // If grade returned an `Option<&Vec<String>>`,
+//     // the internal implementation would be forced to keep a `Vec<String>` to lend out.
+//     // By returning an owned vector instead,
+//     // the internal implementation is free to use whatever it chooses.
+//     pub fn grade(&self, grade: u32) -> Option<Vec<String>> {
+//         self.grades.get(&grade).map(|k| k.iter().cloned().collect())
+//     }
+// }
