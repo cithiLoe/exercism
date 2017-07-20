14802cdce1f1cf2c06c992c3d5159a40363e8433
Daniel Spirin <l1ks.sds@gmail.com>
2017-07-20 12:26:44 +0300

    Implement 'robot simulator'

--- rust/robot-simulator/src/lib.rs
+++ rust/robot-simulator/src/lib.rs
@@ -9,36 +9,109 @@ pub enum Direction {
     West,
 }
 
-pub struct Robot;
+pub struct Robot {
+    x: isize,
+    y: isize,
+    direction: Direction,
+}
+
+impl Direction {
+    fn turn_right(self) -> Self {
+        match self {
+            Direction::North => Direction::East,
+            Direction::East => Direction::South,
+            Direction::South => Direction::West,
+            Direction::West => Direction::North,
+        }
+    }
+
+    fn turn_left(self) -> Self {
+        match self {
+            Direction::North => Direction::West,
+            Direction::West => Direction::South,
+            Direction::South => Direction::East,
+            Direction::East => Direction::North,
+        }
+    }
+}
+
+
 
 impl Robot {
     #[allow(unused_variables)]
     pub fn new(x: isize, y: isize, d: Direction) -> Self {
-        unimplemented!()
+        Robot {
+            x: x,
+            y: y,
+            direction: d,
+        }
     }
 
     pub fn turn_right(self) -> Self {
-        unimplemented!()
+        Robot {
+            x: self.x,
+            y: self.y,
+            direction: self.direction.turn_right(),
+        }
     }
 
     pub fn turn_left(self) -> Self {
-        unimplemented!()
+        Robot {
+            x: self.x,
+            y: self.y,
+            direction: self.direction.turn_left(),
+        }
     }
 
     pub fn advance(self) -> Self {
-        unimplemented!()
+        match self.direction {
+            Direction::North => {
+                Robot {
+                    x: self.x,
+                    y: self.y + 1,
+                    direction: self.direction,
+                }
+            }
+            Direction::South => {
+                Robot {
+                    x: self.x,
+                    y: self.y - 1,
+                    direction: self.direction,
+                }
+            }
+            Direction::East => {
+                Robot {
+                    x: self.x + 1,
+                    y: self.y,
+                    direction: self.direction,
+                }
+            }
+            Direction::West => {
+                Robot {
+                    x: self.x - 1,
+                    y: self.y,
+                    direction: self.direction,
+                }
+            }
+        }
     }
 
-    #[allow(unused_variables)]
     pub fn instructions(self, instructions: &str) -> Self {
-        unimplemented!()
+        instructions
+            .chars()
+            .fold(self, |robot, instruction| match instruction {
+                'R' => robot.turn_right(),
+                'L' => robot.turn_left(),
+                'A' => robot.advance(),
+                _ => robot,
+            })
     }
 
     pub fn position(&self) -> (isize, isize) {
-        unimplemented!()
+        (self.x, self.y)
     }
 
     pub fn direction(&self) -> &Direction {
-        unimplemented!()
+        &self.direction
     }
 }
