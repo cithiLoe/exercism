2e06191c5e6d933252bd943b0e446b9261093805
Daniel Spirin <l1ks.sds@gmail.com>
2017-07-24 19:18:20 +0300

    Implement 'space age'

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,18 +1,20 @@
 // The code below is a stub. Just enough to satisfy the compiler.
 // In order to pass the tests you can add-to or change any of this code.
-#![allow(unused_variables)]
 
-pub struct Duration;
+pub struct Duration {
+    seconds: f64,
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!()
+        Duration { seconds: s as f64 }
     }
 }
 
 pub trait Planet {
+    fn get_orbit_duration() -> f64;
     fn years_during(d: &Duration) -> f64 {
-        unimplemented!();
+        d.seconds / Self::get_orbit_duration() / (3600. * 24. * 365.25)
     }
 }
 
@@ -25,11 +27,50 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    fn get_orbit_duration() -> f64 {
+        0.2408467
+    }
+}
+
+impl Planet for Venus {
+    fn get_orbit_duration() -> f64 {
+        0.61519726
+    }
+}
+
+impl Planet for Earth {
+    fn get_orbit_duration() -> f64 {
+        1.0
+    }
+}
+
+impl Planet for Mars {
+    fn get_orbit_duration() -> f64 {
+        1.8808158
+    }
+}
+
+impl Planet for Jupiter {
+    fn get_orbit_duration() -> f64 {
+        11.862615
+    }
+}
+
+impl Planet for Saturn {
+    fn get_orbit_duration() -> f64 {
+        29.447498
+    }
+}
+
+impl Planet for Uranus {
+    fn get_orbit_duration() -> f64 {
+        84.016846
+    }
+}
+
+impl Planet for Neptune {
+    fn get_orbit_duration() -> f64 {
+        164.79132
+    }
+}
