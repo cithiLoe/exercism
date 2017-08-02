9874336def2a158aac4ab95314d955cf32d6f291
Daniel Spirin <l1ks.sds@gmail.com>
2017-08-02 17:13:32 +0300

    Implement 'pyhtagorean triplet'

---
+++ rust/pythagorean-triplet/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn find() -> Option<u32> {
+
+    for i in 1..1000 {
+        for j in (i + 1)..(1000 - i) {
+            let k = 1000 - (i + j);
+            if i * i + j * j == k * k {
+                return Some(i * j * k);
+            }
+        }
+    }
+    None
+}
